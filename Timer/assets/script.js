59defbea3efb38f50b1e8513ddb559a5ba401ed6
romain-de-javel <73030257+romain-de-javel@users.noreply.github.com>
2021-09-16 00:21:26 +0200

    Add files via upload

---
+++ Timer/assets/script.js
@@ -0,0 +1,30 @@
+function timer(){
+    const currentDate  = new Date();
+    const newYearDate = new Date("1 Jan 2022");
+    const total_sec =(newYearDate - currentDate)/1000;
+    
+
+    const day = Math.floor(total_sec/3600/24);
+    const hours = Math.floor(total_sec/3600)%24;
+    const mins = Math.floor(total_sec/60)%60;
+    const secs = Math.floor(total_sec)%60;
+
+    document.getElementById('day').innerHTML=day;
+    document.getElementById('hour').innerHTML=format_time(hours);
+    document.getElementById('min').innerHTML=format_time(mins);
+    document.getElementById('sec').innerHTML=format_time(secs);
+}
+function format_time(time){
+    return time <10 ? (`0${time}`) : time;
+}
+
+setInterval(timer, 1000);
+
+
+
+
+
+
+
+
+
